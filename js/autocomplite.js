db00d7213acbbd89bb5411025b84f17453d8eafe
Irina Soboleva <iralso@gmail.com>
2016-01-25 13:29:57 +0300

    first commit

--- js/autocomplite.js
+++ js/autocomplite.js
@@ -1,24 +1,5 @@
 $(document).ready(function(){
-	$( "#input" ).after( "<ul id='result'></ul>" );
-
-	$("#input").keyup(function (e) {
-		var value = $("input").val();
-		if(value.length>2){
-			var url = 'part='+$("input").val();
-			$.ajax({
-				type: 'POST',
-				url: 'http://prep.snegoffon.ru/Json/Preps',
-				cache: false,
-				data: url,
-				success: function(data){
-					$('#result li').remove();
-					for(var i=0; i<data.length; i++) {
-						$('#result').append('<li>'+data[i].Name+'</li>');
-					}
-				}
-			});
-		}
-	});
+	
 
 	var start = 'part=';
 	$.ajax({
@@ -32,10 +13,30 @@ $(document).ready(function(){
 			}
 		}
 	});
-	$('#result').on('click', 'li', function(){
-		$('#input').val($(this).html());
-	});
 });
-function autocomplite (value) {
-	alert(value);
+function autocomplite (input) {
+	input.after( "<ul id='"+input+"-result'></ul>" );
+
+	input.keyup(function (e) {
+		var value = input.val();
+		if(value.length>2){
+			var url = 'part='+input.val();
+			$.ajax({
+				type: 'POST',
+				url: 'http://prep.snegoffon.ru/Json/Preps',
+				cache: false,
+				data: url,
+				success: function(data){
+					$('#'+input+'-result li').remove();
+					for(var i=0; i<data.length; i++) {
+						$('#'+input+'-result').append('<li>'+data[i].Name+'</li>');
+					}
+				}
+			});
+		}
+	});
+
+	$('#'+input+'result').on('click', 'li', function(){
+		input.val($(this).html());
+	});
 }
